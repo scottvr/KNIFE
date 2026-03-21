472f70159bbbff06f171c575df162841088d091d
ScottVR <scottvr@gmail.com>
2026-03-21 07:24:40 -0500

    fugg

--- src/constants/palettes.ts
+++
@@ -1,9 +0,0 @@
-import { Palette } from "../types/fractal";
-
-export const PALETTES: Palette[] = [
-  { id: 0, name: "Cosmic" },
-  { id: 1, name: "Fire" },
-  { id: 2, name: "Plasma" },
-  { id: 3, name: "Neon" },
-  { id: 4, name: "Silver" }
-];
